19fb0266498cd6e41250192e2cea907692f5ab48
Ryan <zelda0169@gmailcom>
2020-09-28 14:54:36 -0600

    Add project files.

---
+++ Integers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    public class Integers
+    {
+        public int myMethod(int x, int y = 10)
+        {
+            int result = x * y;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using MathClass;
+using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number .");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now Pick another number if you want, if not push enter");
+            var num2 = (Console.ReadLine());
+            
+            Integers intobj1 = new Integers();
+            if (num2 == "")
+            {
+                int result = intobj1.myMethod(num1);
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            else
+            {
+                int num3 = Convert.ToInt32(num2);
+                int result2 = intobj1.myMethod(num1, num3);
+                Console.WriteLine(result2);
+                Console.ReadLine();
+            }
+           }
+    }
+}
